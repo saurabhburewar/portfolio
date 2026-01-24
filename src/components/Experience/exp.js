a4917987e609c4dde079a89eba85d4f454f82029
Saurabh Burewar <saurabh.skbr@gmail.com>
2026-01-24 16:30:37 +0530

    Update exp.js

--- src/components/Experience/exp.js
+++ src/components/Experience/exp.js
@@ -22,24 +22,24 @@ export default function Experience() {
                 <div className="expLine"></div>
                 <div className="expCurrentbox">
                     <div className="expLogos">
-                        <img src={deloitteimg} onClick={() => {setComp(data.deloitte)}}></img>
-                        <div className={`${comp==data.deloitte ? "expActive" : null}`}></div>
+                        <img src={deloitteimg} onClick={() => {setComp(data.deloitte)}} alt="Deloitte"></img>
+                        <div className={`${comp===data.deloitte ? "expActive" : null}`}></div>
                     </div>
                     <div className="expDates"><b>Data Engineer</b></div>
                     <div className="expDates"><i>2022 - Present</i></div>
                 </div>
                 <div className="expPastbox1">
                     <div className="expLogos">
-                        <img src={finezzaimg} onClick={() => {setComp(data.finezza)}}></img>
-                        <div className={`${comp==data.finezza ? "expActive" : null}`}></div>
+                        <img src={finezzaimg} onClick={() => {setComp(data.finezza)}} alt="Finezza"></img>
+                        <div className={`${comp===data.finezza ? "expActive" : null}`}></div>
                     </div>
                     <div className="expDates expPast"><b>Product Manager Intern</b></div>
                     <div className="expDates expPast"><i>May 2021 - Jul 2021</i></div>
                 </div>
                 <div className="expPastbox2">
                     <div className="expLogos">
-                        <img src={upraisedimg} onClick={() => {setComp(data.upraised)}}></img>
-                        <div className={`${comp==data.upraised ? "expActive" : null}`}></div>
+                        <img src={upraisedimg} onClick={() => {setComp(data.upraised)}} alt="Upraised"></img>
+                        <div className={`${comp===data.upraised ? "expActive" : null}`}></div>
                     </div>
                     <div className="expDates expPast"><b>Product Manager Trainee</b></div>
                     <div className="expDates expPast"><i>Oct 2020 - Jan 2021</i></div>
@@ -53,3 +53,4 @@ export default function Experience() {
         </div>
     )
 }
+
